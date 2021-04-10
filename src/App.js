88c682a1d13cc1cb91d59053f568d7866d1eb05c
Dulan Wirajith <dulan@zeptolytics.com>
2021-04-10 07:29:55 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import HeaderComponent from './components/header/HeaderComponent';
+
+
+function App() {
+  return (
+    <Provider store={store}>
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        
+      </header> */}
+        <HeaderComponent></HeaderComponent>
+
+    </div>
+    </Provider>
+
+  );
+}
+
+export default App;
