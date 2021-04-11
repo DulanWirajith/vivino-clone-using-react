b8304448c83bb9aa0f61ef09d972c7d939c033b8
Dulan Wirajith <dulanwirajith1995@gmail.com>
2021-04-11 14:00:57 +0530

    body content done

--- src/pages/VivinoPage.js
+++ src/pages/VivinoPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import FiltersShowingComponent from "../components/filters-showing/FiltersShowingComponent";
 import HeaderComponent from "../components/header/HeaderComponent";
 import SecondHeader from "../components/second-header/SecondHeader";
 import "./styles.scss";
@@ -9,6 +10,14 @@ function VivinoPage(props) {
       <div className="header">
         <HeaderComponent></HeaderComponent>
         <SecondHeader></SecondHeader>
+      </div>
+
+      <div className="body-content">
+        <h2 className="showing-text">
+          Showing 2565 Red wines between $20 - $80 rated above 3.5 stars
+        </h2>
+
+        <FiltersShowingComponent></FiltersShowingComponent>
       </div>
     </div>
   );
