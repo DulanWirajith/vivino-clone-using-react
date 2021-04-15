8a6a489948d0d75259de929462fb4aeb80b11be2
Dulan Wirajith <dulanwirajith1995@gmail.com>
2021-04-15 20:01:11 +0530

    done and dusted

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import HeaderComponent from './components/header/HeaderComponent';
-import VivinoPage from './pages/VivinoPage';
-
+import VivinoPage from "./pages/VivinoPage";
 
 function App() {
   return (
     <Provider store={store}>
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header> */}
-        {/* <HeaderComponent></HeaderComponent> */}
+      <div className="App">
         <VivinoPage></VivinoPage>
-    </div>
+      </div>
     </Provider>
-
   );
 }
 
